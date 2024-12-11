69eb72fea765aec6bfe2e3a8aa1edcd5ea42531a
woo-be <thsckddn100@gmail.com>
2024-12-11 17:17:04 +0900

    add user domain

---
+++ src/main/java/com/practice/domain/user/SocialType.java
@@ -0,0 +1,5 @@
+package com.practice.domain.user;
+
+public enum SocialType {
+  KAKAO, NAVER, GOOGLE
+}
---
+++ src/main/java/com/practice/domain/user/User.java
@@ -0,0 +1,61 @@
+package com.practice.domain.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Builder
+@Table(name = "USERS")
+@AllArgsConstructor
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "user_id")
+  private Long id;
+
+  private String email;
+  private String password;
+  private String nickname;
+  private String imageUrl;
+  private int age;
+  private String city;
+
+  @Enumerated(EnumType.STRING)
+  private Role role;
+
+  @Enumerated(EnumType.STRING)
+  private SocialType socialType; // KAKAO, NAVER, GOOGLE
+
+  private String socialId; // 로그인한 소셜 타입의 식별자 값 (일반 로그인인 경우 null)
+
+  private String refreshToken; // 리프레시 토큰
+
+  // 유저 권한 설정 메소드
+  public void authorizeUser() {
+    this.role = Role.USER;
+  }
+
+  // 비밀번호 암호화 메소드
+  public void passwordEncode(PasswordEncoder passwordEncoder) {
+    this.password = passwordEncoder.encode(this.password);
+  }
+
+  public void updateRefreshToken(String updateRefreshToken) {
+    this.refreshToken = updateRefreshToken;
+  }
+}
