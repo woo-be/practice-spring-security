9764604417067ccc2992b41a76fcf9c66bb99849
woo-be <thsckddn100@gmail.com>
2024-12-12 15:23:50 +0900

    modify comment

--- src/main/java/com/practice/domain/user/User.java
+++ src/main/java/com/practice/domain/user/User.java
@@ -43,14 +43,12 @@ public class User {
 
   private String socialId; // 로그인한 소셜 타입의 식별자 값 (일반 로그인인 경우 null)
 
-  private String refreshToken; // 리프레시 토큰
+  private String refreshToken;
 
-  // 유저 권한 설정 메소드
   public void authorizeUser() {
     this.role = Role.USER;
   }
 
-  // 비밀번호 암호화 메소드
   public void passwordEncode(PasswordEncoder passwordEncoder) {
     this.password = passwordEncoder.encode(this.password);
   }
