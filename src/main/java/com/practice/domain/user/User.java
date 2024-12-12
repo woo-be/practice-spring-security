3d77da7e4f73d61a26268565cf5a923aa0b67063
woo-be <thsckddn100@gmail.com>
2024-12-12 15:27:41 +0900

    CLI commit test
    test

--- src/main/java/com/practice/domain/user/User.java
+++ src/main/java/com/practice/domain/user/User.java
@@ -41,7 +41,7 @@ public class User {
   @Enumerated(EnumType.STRING)
   private SocialType socialType; // KAKAO, NAVER, GOOGLE
 
-  private String socialId; // 로그인한 소셜 타입의 식별자 값. (일반 로그인인 경우 null)
+  private String socialId; // 로그인한 소셜 타입의 식별자 값 (일반 로그인인 경우 null)
 
   private String refreshToken;
 
